c7f75ad50a0ccb6a7aeed54b85b19a075d3ac791
Arthur <thurrio@gmail.com>
2020-02-18 17:15:53 -0300

    alling inputs

--- script.js
+++ script.js
@@ -1,40 +1,38 @@
-function turnBinaryOnDecimal(){
+function turnBinaryOnDecimal() {
 
     var valor = document.getElementById("binary").value;
 
-    if(valor.slice(-1) > 1) {
+    if (valor.slice(-1) > 1) {
 
-        document.getElementById("binary").value=document.getElementById("binary").value
-        .substring(0,valor.length-1);
+        document.getElementById("binary").value = document.getElementById("binary").value
+            .substring(0, valor.length - 1);
 
         return;
     }
-        
-    console.log("Binario:" + valor);
 
-    document.getElementById("decimal").value = 
-    bin2dec(valor,valor.length);
-    
+    document.getElementById("decimal").value =
+        bin2dec(valor, valor.length);
+
 }
 
 // Return decimal number
-function bin2dec(binary, position){
+function bin2dec(binary, position) {
 
-    var length = binary.length-1;
+    var length = binary.length - 1;
 
-    if(position==0){
-        return binary.charAt(length-position) * power(2,position);
+    if (position == 0) {
+        return binary.charAt(length - position) * power(2, position);
     }
 
-    return binary.charAt(length-position) *
-     power(2,position) + bin2dec(binary, position-1); 
+    return binary.charAt(length - position) *
+        power(2, position) + bin2dec(binary, position - 1);
 }
 
-function power(number,elevate) {
+function power(number, elevate) {
 
-    if(elevate==0){
+    if (elevate == 0) {
         return 1;
     }
-    
-    return number * power(number,elevate-1);
+
+    return number * power(number, elevate - 1);
 }
