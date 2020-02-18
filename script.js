0dbdb7b2f08c3a72cb71662b7a0a54d2ca40413c
Arthur <thurrio@gmail.com>
2020-02-18 16:37:06 -0300

    Create validate input

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-alert("Olá mundo")
+function turnBinaryOnDecimal(){
+
+    var valor = document.getElementById("binary").value;
+
+    if(valor.slice(-1) > 1) {
+
+        document.getElementById("binary").value=document.getElementById("binary").value
+        .substring(0,valor.length-1);
+
+        return;
+    }
+        
+
+
+    document.getElementById("decimal").value = 
+    valor;
+    
+    
+
+
+}
