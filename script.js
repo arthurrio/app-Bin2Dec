63384044e965cd178ee119bb6f7d85b5315d23b6
Arthur <thurrio@gmail.com>
2020-02-18 17:05:09 -0300

    Creating function to convert binary into decimal

--- script.js
+++ script.js
@@ -10,12 +10,31 @@ function turnBinaryOnDecimal(){
         return;
     }
         
-
+    console.log("Binario:" + valor);
 
     document.getElementById("decimal").value = 
-    valor;
+    bin2dec(valor,valor.length);
     
+}
+
+// Return decimal number
+function bin2dec(binary, position){
+
+    var length = binary.length-1;
+
+    if(position==0){
+        return binary.charAt(length-position) * power(2,position);
+    }
+
+    return binary.charAt(length-position) *
+     power(2,position) + bin2dec(binary, position-1); 
+}
+
+function power(number,elevate) {
+
+    if(elevate==0){
+        return 1;
+    }
     
-
-
+    return number * power(number,elevate-1);
 }
